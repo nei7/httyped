f6b204fce06081a82a77f2bf8750d063d7db19b0
nei7 <franciszek.szarek2@gmail.com>
2022-07-30 20:22:16 +0200

    feat: add eslint, unbuild

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
